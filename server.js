232b7276061c5ca37dd0d433902e2ec098c1a28e
Felipe Alpino <felipealpino1@gmail.com>
2021-03-23 15:23:07 -0300

    configurando mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const app = require('./app');
+const mongoose = require('mongoose')
 
 require('dotenv').config({path:'variables.env'});
+
+//DATABASE CONNECTION 
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = global.Promise //pode user ES6 - await, Promises
+mongoose.connection.on('error', (error) => {
+    console.error("Erro: "+error.message);
+})
 
 app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
