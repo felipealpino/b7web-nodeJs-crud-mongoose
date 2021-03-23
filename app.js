8214c47507fb3ef2feed5a314cf4f0a92b0220c7
Felipe Alpino <felipealpino1@gmail.com>
2021-03-23 16:52:04 -0300

    Template Helpers - helpers.js

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const mustache = require('mustache-express')
 const router = require('./routes/index')
+const helpers = require('./helpers')
 
 //Configurações 
 const app = express();
-app.use('/', router)
+app.use((req,res,next)=> { //tem que ser antes da definição das rotas 
+    res.locals.helpers = helpers;
+    res.locals.teste = 'testando123testando';
+    next();
+})
 
+app.use('/', router)
+  
 app.use(express.json()) //body-parser (old library)
 
 app.engine('mst', mustache(__dirname+'/views/partials', '.mst')) // extensão do arquivo, rodar a varaivel como função
--- routes/index.js
+++ routes/index.js
@@ -3,34 +3,14 @@ const express = require('express')
 //Rotas 
 const router = express.Router();
 
-    
-
 router.get('/', (req,res) => { 
     
     const obj = {
-        'nome':'Felipe',
-        'idade':25, 
-        'sobrenome':'Gontijo', 
-        'profissao':'estudante',
-        'empregado': true,
-        'materiais': [
-            {
-                nome:'Lapis',
-                quant:2,
-            }, 
-            {
-                nome:'Borracha',
-                quant:1,
-            },
-            {
-                nome:'Caneta', 
-                quant:5, 
-            }, 
-        ],
-        'interesses': ['node', 'js', 'laravel', 'php']
+        nome:'Felipe',
+        pageTitle: 'Titulo preenchido',  
     }
 
     res.render('home', obj); //segundo parametro seria os dados para enviar para /views/home
 })
 
-module.exports = router;  
+module.exports = router;   
