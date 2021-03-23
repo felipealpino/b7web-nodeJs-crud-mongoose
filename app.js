079ebc93ecda390a1608093fd15e199357747aa3
Felipe Alpino <felipealpino1@gmail.com>
2021-03-23 16:05:38 -0300

    Configurando template engine -- mustache

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const mustache = require('mustache-express')
 const router = require('./routes/index')
-
 
 //Configurações
 const app = express();
 app.use('/', router)
-app.use('/sobre', router)
-
 
 app.use(express.json()) //body-parser (old library)
+
+app.engine('mst', mustache()) // extensão do arquivo, rodar a varaivel como função
+app.set('view engine', 'mst');
+app.set('views', __dirname+'/views') // __dirname = diretório absoluto
+
+
 module.exports = app; 
--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,13 @@ const express = require('express')
 
 //Rotas 
 const router = express.Router();
-router.get('/', (req,res) => {
-    // /?nome=Felipe&idade=25
-    let nome = req.query.nome
-    let idade = req.query.idade
-
-    res.json(req.query)
+router.get('/', (req,res) => { 
+    res.render('home', {
+        'nome':'Felipe',
+        'idade':25, 
+        'sobrenome':'Gontijo', 
+        'profissao':'estudante'
+    }); //segundo parametro seria os dados para enviar para /views/home
 })
 
-router.get('/posts/:id', (req,res) => {
-    let id = req.params.id
-    res.send('Id do post'+ id) 
-})
-
-router.get('/sobre', (req, res) => {
-    res.send('Pagina sobre');
-})
-
-module.exports = router; 
+module.exports = router;
